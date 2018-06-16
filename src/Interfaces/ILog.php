26c03523db6036bcb5353c48c2cb1083f4ad9e15
Joao Moraes <joaomoraesbr@gmail.com>
2018-06-16 13:23:42 -0300

    add interface methods

--- src/Interfaces/ILog.php
+++ src/Interfaces/ILog.php
@@ -2,8 +2,16 @@
 
 namespace FcPhp\Log\Interfaces
 {
+	use FcPhp\Log\Interfaces\ILog;
+	
 	interface ILog
 	{
-		
+		public static function getInstance(string $directoryOutput, $dateFormat = 'Y-m-d H:i:s', string $extension = 'log', bool $debug = false) :ILog;
+
+		public function __construct(string $directory, string $dateFormat = 'Y-m-d H:i:s', string $extension = 'log', bool $debug = false);
+
+		public function __call($method, array $args = []);
+
+		public function write(string $fileName, string $logText);
 	}
 }
